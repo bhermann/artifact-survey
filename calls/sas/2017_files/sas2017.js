e4f494c39bafe5a9d44abc8ad0b31bede57f0d4a
Anonymous User <anonymous@university.org>
2019-09-03 11:48:46 +0200

    Added missing html versions

---
+++ calls/sas/2017_files/sas2017.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170706
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,270),url:'sas2017_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'sas2017_files/stroke_1.png'},{rect:new IWRect(1,-1,126,2),url:'sas2017_files/stroke_2.png'},{rect:new IWRect(127,-1,3,2),url:'sas2017_files/stroke_3.png'},{rect:new IWRect(127,1,3,270),url:'sas2017_files/stroke_4.png'},{rect:new IWRect(127,271,3,2),url:'sas2017_files/stroke_5.png'},{rect:new IWRect(1,271,126,2),url:'sas2017_files/stroke_6.png'},{rect:new IWRect(-1,271,2,2),url:'sas2017_files/stroke_7.png'}],new IWSize(128,272))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('sas2017_files/sas2017Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
---
+++ calls/sas/2018_files/sas2018.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20180825
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,62),url:'sas2018_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'sas2018_files/stroke_1.png'},{rect:new IWRect(1,-1,179,2),url:'sas2018_files/stroke_2.png'},{rect:new IWRect(180,-1,3,2),url:'sas2018_files/stroke_3.png'},{rect:new IWRect(180,1,3,62),url:'sas2018_files/stroke_4.png'},{rect:new IWRect(180,63,3,2),url:'sas2018_files/stroke_5.png'},{rect:new IWRect(1,63,179,2),url:'sas2018_files/stroke_6.png'},{rect:new IWRect(-1,63,2,2),url:'sas2018_files/stroke_7.png'}],new IWSize(182,64))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('sas2018_files/sas2018Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
