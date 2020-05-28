4aa262dceb5dfa83085e7bfaca0fc54690a975df
Ben Hermann <ben.hermann@googlemail.com>
2020-05-28 18:07:45 +0200

    First part of the artifact

---
+++ analysis/survey/conferencespread.R
@@ -0,0 +1,49 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tibble)
+
+# Read in the complete survey result data
+SurveyData <- read_excel("../../results/results-survey54231.xlsx")
+
+# trim data to the same set we tagged
+SurveyData <- SurveyData %>% filter(id <= 273)
+
+# remove unusable replies from ID 99 and 218
+SurveyData <- SurveyData %>% filter(id != 99 & id != 218)
+
+# The sequences of conferences as asked in the survey
+Conferences <- list("ICSE", "FSE","ISSTA","SAS","VISSOFT","OOPSLA","ECOOP","POPL","PLDI","SLE", "PPoPP","ICFP","CGO", "MODELS", "CAV", "TACAS")
+
+# Tabulates the summarized results by conference/year
+ConferenceMatrix <-
+  rename(
+    add_column(
+      as.data.frame(do.call(rbind, split.default(colSums(select(SurveyData, `g0[CICSE_Y2011]`:`g0[CTACAS_Y2019]`), na.rm = TRUE), rep(1:16, each=9)))),
+      ConferenceId = 1:16, Conference = unlist(Conferences), .before = 1),
+    `2011` = `g0[CICSE_Y2011]`, `2012` = `g0[CICSE_Y2012]`, `2013` = `g0[CICSE_Y2013]`,
+    `2014` = `g0[CICSE_Y2014]`, `2015` = `g0[CICSE_Y2015]`, `2016` = `g0[CICSE_Y2016]`,
+    `2017` = `g0[CICSE_Y2017]`, `2018` = `g0[CICSE_Y2018]`, `2019` = `g0[CICSE_Y2019]`
+    )
+
+CommitteeData <- rename(separate(tally(group_by(read_excel("../../results/aec.xlsx"), Conference)),
+                          Conference, sep = " ", into = c("Conference", "year")),
+                        `committee_size` = `n`)
+
+
+ConferencePlotData <- filter(gather(ConferenceMatrix, key="year", value="value", `2011`:`2019`), value > 0)
+FullPlotData <- mutate(full_join(CommitteeData, ConferencePlotData, by = c("Conference", "year")),
+                       committee_size = ifelse(is.na(committee_size), 0, committee_size),
+                       value = ifelse(is.na(value), 0, value),
+                       ConferenceId = ifelse(is.na(ConferenceId), match(Conference, Conferences), ConferenceId))
+
+lconf <- function(index) { return (Conferences[index]) }
+ConferencePlot <- ggplot(FullPlotData, aes(x = year, y = ConferenceId)) +
+  geom_point(aes(size = (committee_size)), color = "green", alpha=.4, show.legend = FALSE) +
+  geom_point(aes(size = (ifelse(value == 0, NA, value))), alpha=.7, color = "red", show.legend = FALSE) +
+  geom_text(aes(label = paste(value, "/", committee_size)), size = 2.5, position = position_nudge(y = -0.2, x = 0.32)) +
+  scale_y_discrete(limits = 1:16, breaks = 1:16, labels = lconf, drop = FALSE, position = "right", name = "") +
+  scale_size_area(max_size=15)
+#show(ConferencePlot)
+ggsave(ConferencePlot, filename="output/ConferencePlot.pdf")
---
+++ analysis/survey/participant_stats.R
@@ -0,0 +1,36 @@
+# This script produces a plot of the histogram of individuals by number of AECs served in partioned by invited and responded
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+
+SurveyData <- read_excel("../../results/results-survey54231.xlsx")
+
+# trim data to the same set we tagged
+SurveyData <- SurveyData %>% filter(id <= 273)
+
+# remove unusable replies from ID 99 and 218
+SurveyData <- SurveyData %>% filter(id != 99 & id != 218)
+
+survey_hist_data <- as.data.frame(rowSums(SurveyData %>% select(`g0[CICSE_Y2011]`:`g0[CTACAS_Y2019]`), na.rm = TRUE))
+names(survey_hist_data) <- "served_in"
+
+survey_hist_data <- survey_hist_data %>% group_by(served_in) %>% tally %>% filter(served_in > 0)
+
+aec <- read_excel("../../results/aec.xlsx")
+
+aec_hist_data <- aec %>% group_by(`Full name`) %>% tally %>% rename(served_in = n) %>% group_by(served_in) %>% tally %>% rbind(c(7, 0)) %>% arrange(served_in)
+
+full_hist_data <- aec_hist_data %>% dplyr::left_join(survey_hist_data, by="served_in")  %>% replace_na(list(n.y = 0)) %>% rename(invited=n.x, responded=n.y)
+
+full_plot_data <- full_hist_data %>% gather(var, val, invited, responded)
+
+aec_hist_plot <- ggplot(data = full_plot_data, aes(x=served_in, y=val, fill=var)) +
+  scale_x_discrete(limits = c(1:10)) +
+  geom_bar(position="dodge", stat = "identity") +
+  geom_text(position=position_dodge(width=0.9), aes(label=val), vjust=-0.8, color="black", size=3.5) +
+  labs(x = "Committees served in", y = "Number of individuals", fill="") +
+  theme_minimal()+ theme(legend.position = c(0.8, 0.8))
+
+ggsave(aec_hist_plot, filename="output/aec_histogram.pdf")
